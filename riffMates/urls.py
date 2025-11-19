19dacbefccffdfb2d2bea6c711728e3efc8dcca3
yeti2391 <marco2391@gmail.com>
2025-11-18 22:10:46 -0300

    se ajusta codigo al libro, se cambia la ruta a home/views.py

--- riffMates/urls.py
+++ riffMates/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from home import views as home_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls')),
+    #path('', include('home.urls')),
+    path('credits/', home_views.credits, name='credits'),
 ]
